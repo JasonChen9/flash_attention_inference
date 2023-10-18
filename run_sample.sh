83ec1b526b66c66bc62819cc601feb8d7fd97f55
JasonChen9 <995975102@qq.com>
2023-10-18 12:43:50 +0900

    add: 2080Ti test data

--- run_sample.sh
+++ run_sample.sh
@@ -15,14 +15,14 @@ rm -rf log ncu && mkdir -p log ncu
 # $1: b. $2: sq, $3: sk, $4: h, $5: d, $6: is_causal
 evaluate_fai() {
     echo "Evaluating $1 * $2 * $3 * $4 * $5 * $6"
-    $WORK_PATH/output/bin/flash_attention_inference -b=$1 -sq=$2 -sk=$3 -h=$4 -d=$5 -is_causal=$6 -num_splits=0 -warmup_iterations=1 -profiling_iterations=10 -sleep_duration=100 -enable_check=false > log/fai_${1}_${2}_${3}_${4}_${5}.log 2>&1
+    $WORK_PATH/build/flash_attention_inference -b=$1 -sq=$2 -sk=$3 -h=$4 -d=$5 -is_causal=$6 -num_splits=0 -warmup_iterations=1 -profiling_iterations=10 -sleep_duration=100 -enable_check=false > log/fai_${1}_${2}_${3}_${4}_${5}.log 2>&1
     sleep 3
 }
 
-benchmark_fai_generator_batch() {
-    echo "Evaluating Generator Batch"
+benchmark_batch() {
+    echo "Evaluating Batch"
     batch=(1 2 4 8 16 32 64 128 256 512 768 1024 1536 2048)
-    sq=1
+    sq=128
     sk=128
     h=32
     d=128
@@ -33,53 +33,23 @@ benchmark_fai_generator_batch() {
     done
 }
 
-benchmark_fai_generator_seq() {
-    echo "Evaluating Generator Seq"
+benchmark_seq() {
+    echo "Evaluating Seq"
     b=128
-    sq=1
     seq_k=(1 2 4 8 16 32 64 128 256 512 768 1024 1536 2048)
     h=32
     d=128
 
     for sk in ${seq_k[@]};
     do
-        evaluate_fai $b $sq $sk $h $d false
+        evaluate_fai $b $sk $sk $h $d false
     done
 }
 
-benchmark_fai_prompt_batch() {
-    echo "Evaluating Prompt Batch"
-    batch=(1 2 4 8 16 32 64 128 256 512 768 1024 1536 2048)
-    s=128
-    h=32
-    d=128
-
-    for b in ${batch[@]};
-    do
-        evaluate_fai $b $s $s $h $d true
-    done
-}
-
-benchmark_fai_prompt_seq() {
-    echo "Evaluating Prompt Seq"
-    b=128
-    seq=(1 2 4 8 16 32 64 128 256 512 768 1024 1536 2048)
-    h=32
-    d=128
-
-    for s in ${seq[@]};
-    do
-        evaluate_fai $b $s $s $h $d true
-    done
-}
 
 benchmark_fai() {
-    benchmark_fai_prompt_seq
-    benchmark_fai_prompt_batch
-    benchmark_fai_generator_seq
-    benchmark_fai_generator_batch
+    # benchmark_seq
+    benchmark_batch
 }
 
-nohup $WORK_PATH/output/bin/flash_attention_inference -b=2 -sq=256 -sk=256 -h=32 -d=128 -is_causal=true -num_splits=0 -warmup_iterations=1 -profiling_iterations=10 -sleep_duration=100 -enable_check=true > log/fai_2_256_256_32_128.log 2>&1 &
-# sudo ncu --set full --target-processes all --force-overwrite -o ncu/fai_2_256_256_32_128 $WORK_PATH/output/bin/flash_attention_inference -b=2 -sq=256 -sk=256 -h=32 -d=128 -is_causal=true -num_splits=0 -warmup_iterations=1 -profiling_iterations=1 -sleep_duration=100 -enable_check=false > log/ncu_fai_2_256_256_32_128.log 2>&1
-# benchmark_fai
+benchmark_fai
--- tools/performance/performance_seq.py
+++ tools/performance/performance_seq.py
@@ -38,7 +38,7 @@ def draw_line_chart(methods, dims, data, figure_name, y_label, title):
     # plt.hlines(y=100, xmin=dims_str[0], xmax=dims_str[-1], colors='r', linestyles='-.')
     plt.grid(True, linestyle='-.')
 
-    plt.xlabel('batch', fontdict={'size': '30'})
+    plt.xlabel('length', fontdict={'size': '30'})
     plt.ylabel(y_label, fontdict={'size': '30'})
     plt.title(title, fontdict={'size': '30'})
     plt.legend(methods, loc='best', prop={'size': '30'})
@@ -52,7 +52,7 @@ def read_data(methods, dims, data_path, log_files):
     data_performance = np.zeros((len(methods), len(dims)), np.float64)
 
     for log_file in log_files:
-        dim = int((log_file.split('.')[0]).split('_')[1])
+        dim = int((log_file.split('.')[0]).split('_')[2])
         with open(data_path + log_file) as fp:
             line = fp.readline()
             while line:
@@ -73,7 +73,7 @@ def get_dims(log_files):
     dims = []
 
     for log_file in log_files:
-        dims.append(int((log_file.split('.')[0]).split('_')[1]))
+        dims.append(int((log_file.split('.')[0]).split('_')[2]))
 
     dims.sort()
 
@@ -108,16 +108,16 @@ def analyze_data(data_path):
     data_throughput, data_performance = read_data(
         methods, dims, data_path, log_files)
     draw_line_chart(methods, dims, data_throughput, data_path +
-                    'flash_attention_throughput.png', 'Throughput / TFLOPS', 'Flash Attention Throughput(len=128)')
+                    'flash_attention_throughput.png', 'Throughput / TFLOPS', 'Flash Attention Throughput(batch=16)')
     draw_line_chart(methods, dims, data_performance, data_path + 'flash_attention_performance.png',
-                    'Performance Compared with Flash Attention / %', 'Flash Attention Performance(len=128)')
+                    'Performance Compared with Flash Attention / %', 'Flash Attention Performance(batch=16)')
 
 
 def main():
     usage = "python3 performance.py -p/--path log/"
     parser = optparse.OptionParser(usage)
     parser.add_option('-p', '--path', dest='path',
-                      type='string', help='data path', default='log')
+                      type='string', help='data path', default='log/')
 
     options, args = parser.parse_args()
     path = options.path
